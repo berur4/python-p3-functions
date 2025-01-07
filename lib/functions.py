f107504b1acb2474bb9622bcab1d07b60b1b4678
berur4 <berurcaleb@gmail.com>
2025-01-07 15:46:05 +0300

    Python functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
+    
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return num1 + num2
+    result = add(45, 55)
+    print(result)
     pass
 
 def halve(number):
+    return number / 2
+    result = halve(100)
+    print(result)
     pass
